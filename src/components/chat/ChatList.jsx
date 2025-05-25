cf2af322114fa965193209e2e5f100b2f6224bf4
i-am-dahee <hxxly02@gmail.com>
2025-05-26 04:21:48 +0900

    [feat] 채팅 리스트 구현 완료 #7

--- src/components/chat/ChatList.jsx
+++ src/components/chat/ChatList.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/Chat.css";
 
 const ChatList = () => {
+  const [rooms, setRooms] = useState([]);
+  const [selectedIndex, setSelectedIndex] = useState(null);
+
   const handleNewChat = () => {
-    alert("새 채팅");
+    const count = rooms.filter((room) => room.startsWith("새 채팅")).length;
+
+    const newRoomName = `새 채팅 ${count + 1}`;
+    setRooms([newRoomName, ...rooms]);
+    setSelectedIndex(0);
   };
 
   return (
@@ -11,6 +18,22 @@ const ChatList = () => {
       <button className="chat-new-btn" onClick={handleNewChat}>
         +&nbsp;&nbsp;새 채팅
       </button>
+
+      <ul className="chat-list-ul">
+        {rooms.map((room, index) => (
+          <li key={index}>
+            <button
+              className={`chat-room-btn ${
+                selectedIndex === index ? "selected" : ""
+              }`}
+              onClick={() => setSelectedIndex(index)}
+            >
+              <span className="chat-room-icon">💬</span>
+              {room}
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
