030b70b4a8666d232eb14b4dac8c68276922f375
joonyoungchoi0801 <joeplay0801@gmail.com>
2025-06-20 18:35:34 +0900

    fix: 스타일 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import Router from './Router';
 import './App.css';
+import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    const path = location.pathname;
+    if (path === '/register' || path === '/chat' || path === '/demo') {
+      document.body.style.overflowY = 'hidden';
+    } else {
+      document.body.style.overflowY = 'auto';
+    }
+
+    return () => {
+      document.body.style.overflowY = 'auto';
+    };
+  }, [location.pathname]);
   return <Router />;
 }
 
--- src/Router.jsx
+++ src/Router.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './page/home';
 import Layout from './components/Layout';
 import SignUp from './page/signup';
@@ -7,16 +7,14 @@ import Chat from './page/chat';
 
 export default function Router() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/chat" element={<Chat />} />
-          <Route path="/history" element={<Home />} />
-          <Route path="/demo" element={<Demo />} />
-          <Route path="/register" element={<SignUp />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/history" element={<Home />} />
+        <Route path="/demo" element={<Demo />} />
+        <Route path="/register" element={<SignUp />} />
+      </Route>
+    </Routes>
   );
 }
