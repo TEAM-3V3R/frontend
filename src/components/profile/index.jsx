1577cd3bb6fc3dd33a40e1cecdf19ae9814a924b
joonyoungchoi0801 <joeplay0801@gmail.com>
2025-06-22 22:09:11 +0900

    fix: 오류 수정

--- src/components/profile/index.jsx
+++ src/components/profile/index.jsx
@@ -1,5 +1,5 @@
 import Login from './Login';
-import ProfileMenu from './profilemenu';
+import ProfileMenu from './ProfileMenu';
 import styles from './Profile.module.scss';
 function Profile() {
   const isLogin = localStorage.getItem('isLogin') === 'true' || false;
